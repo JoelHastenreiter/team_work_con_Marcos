db8216ba98024de37fdf89098932f8a3413fcbca
JoelHastenreiter <55096566+JoelHastenreiter@users.noreply.github.com>
2019-09-26 19:59:07 -0500

    html-css

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function () {
+    var video = this.document.getElementById("chico");
+    var posVideo = video.offsetTop;
+    var currentScroll = window.pageYOffset;
+});
