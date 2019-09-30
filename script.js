3eb34054442fcba59e83d697b4ad49bd2194558f
Macoy Carmona <macoymarcar@gmail.com>
2019-09-30 09:41:18 -0500

    es el  bueno bueno
    copie y pegue todo

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 window.addEventListener('scroll', function () {
-    var video = this.document.getElementById("chico");
+
+    var video = document.getElementById("video");
+    //video ofset top
     var posVideo = video.offsetTop;
     var currentScroll = window.pageYOffset;
+    var windowheight = window.innerHeight;
+    var alturaVideo = video.offsetHeight;
+
+
+    if (posVideo + alturaVideo < currentScroll + windowheight &&
+        posVideo > currentScroll
+    ) {
+        video.classList.add("play");
+    } else {
+
+        video.classList.remove("play");
+    }
+
+    //console.log("posVideo", posVideo);
+    //console.log("currentScroll", currentScroll);
+    //console.log("windowheight", windowheight);
 });
